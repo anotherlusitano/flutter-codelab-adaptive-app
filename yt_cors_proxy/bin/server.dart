0cb2d441cfbea07ba5c1a6d953835a5007863dac
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-21 11:25:50 +0100

    Update server file

--- yt_cors_proxy/bin/server.dart
+++ yt_cors_proxy/bin/server.dart
@@ -1,29 +1,26 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
+Future<Response> _requestHandler(Request req) async {
+  final target = req.url.replace(scheme: 'https', host: 'i.ytimg.com');
+  final response = await http.get(target);
+  return Response.ok(response.bodyBytes, headers: response.headers);
 }
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  // Configure a pipeline that adds CORS headers and proxies requests.
+  final handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(corsHeaders(headers: {ACCESS_CONTROL_ALLOW_ORIGIN: '*'}))
+      .addHandler(_requestHandler);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
